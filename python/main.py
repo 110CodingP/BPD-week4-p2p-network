72788b5085c0d6415a88eb095735a8a7987f7806
Garima Chauhan <codingp110@gmail.com>
2024-07-23 12:21:53 +0530

    Change the way the code listens to remote node

--- python/main.py
+++ python/main.py
@@ -15,6 +15,8 @@ def main():
     response = requests.get("http://checkip.dyndns.org").text
     ip = re.search("(?:[0-9]{1,3}\.){3}[0-9]{1,3}", response).group()
 
+    print(ip)
+
     # print(response)
 
     # get peers to connect to
@@ -35,7 +37,7 @@ def main():
     except Exception:
         print("Exception occured")
     
-
+    
     # make connection
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -49,7 +51,7 @@ def main():
         except Exception:
             pass
     
-    # print(peer)
+    print(connected_peer)
 
     # do version-verack handshake
       # version msg
@@ -57,8 +59,8 @@ def main():
     services = struct.pack("Q",0)
     timestamp = struct.pack("q",int(time.time()))
     addr_recv_services = struct.pack("Q",1)
-    addr_recv_ip = struct.pack(">16s", bytes.fromhex("00000000000000000000ffff") + socket.inet_aton(peer[0]))
-    addr_recv_port = struct.pack("H",peer[1])
+    addr_recv_ip = struct.pack(">16s", bytes.fromhex("00000000000000000000ffff") + socket.inet_aton(connected_peer[0]))
+    addr_recv_port = struct.pack("H",connected_peer[1])
     addr_trans_services = struct.pack("Q",0)
     addr_trans_ip = struct.pack(">16s",bytes.fromhex("00000000000000000000ffff") + socket.inet_aton(ip))
     addr_trans_port = struct.pack("H",8333)
@@ -97,11 +99,18 @@ def main():
 
     sock.send(version_msg)
     time.sleep(1)
-    recv_msg = sock.recv(8192)
-    print(recv_msg[4:16])
-    if (recv_msg[4:16]==struct.pack(">12s",bytes("version","utf-8"))):
-        sock.send(verack_msg)
-    print(sock.recv(8192)[4:16])
+    # send verack
+    while True:
+        received_header = sock.recv(24)
+        print(received_header)
+        if (not received_header):
+            break
+        else:
+            if (received_header[4:16] == struct.pack("12s",bytes("version","utf-8"))):
+                sock.send(verack_msg)
+                break
+            else:
+                sock.recv(int(received_header[16:20].hex(),base=16))
 
 
     # getdata to get the block
@@ -120,9 +129,7 @@ def main():
 
     getdata_msg = start + command + payload_size + checksum + payload
 
-    sock.send(getdata_msg)
-
-    print(sock.recv(8192)[4:16])
+    # sock.send(getdata_msg)
     
 
 
@@ -143,4 +150,6 @@ if __name__ == "__main__":
    - get block at certain ht: https://bitcoin.stackexchange.com/questions/83990/getting-individual-block-by-height-from-bitcoin-p2p-network
    - message types: https://en.bitcoin.it/wiki/Protocol_documentation#Message_types
    - getting block hash of block 840000: https://mempool.space/block/0000000000000000000320283a032748cef8227873ff4872689bf23f1cda83a5
+   - using a loop to receive messages: https://learnmeabitcoin.com/technical/networking/#keeping-connected
+   - https://stackoverflow.com/questions/38883476/how-to-remove-those-x00-x00
 """
